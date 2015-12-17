69ab0d426c0cf8437c39f77c20e16216e6eee0c8
Ercan <ali.ozcan@infoline-tr.com>
2015-12-17 16:36:17 +0200

    fixed build configuration

--- app/src/main/java/com/apptopus/sampleapp/MainActivity.java
+++ app/src/main/java/com/apptopus/sampleapp/MainActivity.java
@@ -2,12 +2,40 @@ package com.apptopus.sampleapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.apptopus.progressive.Progressive;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button btnToggle;
+    ImageView imgDummy;
+    boolean progress = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnToggle = (Button)findViewById(R.id.btnToggle);
+        imgDummy = (ImageView)findViewById(R.id.imageView);
+        Progressive.showProgress(imgDummy);
+
+        btnToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(progress){
+                    Progressive.hideProgress(imgDummy);
+                    progress = false;
+                }
+                else{
+                    Progressive.showProgress(imgDummy);
+                    progress = true;
+                }
+            }
+        });
     }
 }
