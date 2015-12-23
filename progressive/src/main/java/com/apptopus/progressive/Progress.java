b6f61f7dca843262a66117035b0f1ca5b156f03e
Ercan <ali.ozcan@infoline-tr.com>
2015-12-23 13:49:03 +0200

    added comments and java docs

--- progressive/src/main/java/com/apptopus/progressive/Progress.java
+++ progressive/src/main/java/com/apptopus/progressive/Progress.java
@@ -53,26 +53,34 @@ public class Progress {
         }
     }
 
+    /**
+     * Wraps the view with a frame layout and puts a progress indicator in it
+     */
     private void box(){
         try {
 
-            progressBar =  LayoutInflater.from(view.getContext()).inflate(resourceId, parent, false);
-            FrameLayout.LayoutParams progresLayoutParams =new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
-            progresLayoutParams.gravity = Gravity.CENTER;
-            progressBar.setLayoutParams(progresLayoutParams);
-            parent = (ViewGroup) view.getParent();
-            ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
+            progressBar =  LayoutInflater.from(view.getContext()).inflate(resourceId, parent, false); //inflate the resource
+
+            FrameLayout.LayoutParams progressLayoutParams = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);//create layout params for progress indicator
+            progressLayoutParams.gravity = Gravity.CENTER;//set gravity as center for align in center of view
+            progressBar.setLayoutParams(progressLayoutParams);
+
+            parent = (ViewGroup) view.getParent();//retrieve parent view as a ViewGroup
+            ViewGroup.LayoutParams layoutParams = view.getLayoutParams();//move the exact layout parameter in order to keep position size ect.
             FrameLayout frameLayout = new FrameLayout(view.getContext());
-            frameLayout.setId(view.getId());// to keep RelativeLayout rules
-            frameLayout.setLayoutParams(layoutParams);
-            parent.removeView(view);
-            FrameLayout.LayoutParams frameLayoutParams = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-            frameLayoutParams.gravity = Gravity.CENTER;
+            frameLayout.setId(view.getId());// to keep RelativeLayout rules we give the same id to frame layout
+            frameLayout.setLayoutParams(layoutParams);//passing the previous layout params to frame layout
+
+            parent.removeView(view); //removing the view from its former parent
+
+            FrameLayout.LayoutParams frameLayoutParams = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);//creatin layout params for original view
+            frameLayoutParams.gravity = Gravity.CENTER;//in the center of frame layout
             view.setLayoutParams(frameLayoutParams);
-            frameLayout.addView(view);
-            frameLayout.addView(progressBar);
-            parent.addView(frameLayout,originalPosition);
-            encapsulated = true;
+
+            frameLayout.addView(view);//put original vie in frame layout
+            frameLayout.addView(progressBar);//then overlay progressbar on original view
+            parent.addView(frameLayout,originalPosition);// then replace the original view with new frame layout
+            encapsulated = true;//flag es encapsulated
 
 
 
@@ -83,16 +91,18 @@ public class Progress {
 
     }
 
-
+    /**
+     * unwraps the view from frame layout and removes progressview
+     */
     private void unBox(){
 
-        FrameLayout frameLayout = (FrameLayout) view.getParent();
-        ViewGroup.LayoutParams layoutParams =  frameLayout.getLayoutParams();
-        frameLayout.removeView(view);
+        FrameLayout frameLayout = (FrameLayout) view.getParent();//we know that the parent of original view is a frame layout
+        ViewGroup.LayoutParams layoutParams =  frameLayout.getLayoutParams();//recovers the former layout params
+        frameLayout.removeView(view);//removes the original view from frame layout
         view.setLayoutParams(layoutParams);
-        parent.removeView(frameLayout);
-        parent.addView(view, originalPosition, layoutParams);
-        encapsulated = false;
+        parent.removeView(frameLayout);//removes the frame layout from parent
+        parent.addView(view, originalPosition, layoutParams);//adds back the original view to its original position
+        encapsulated = false;//sets the flag as un-boxed
     }
 
 
